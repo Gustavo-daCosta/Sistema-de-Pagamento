079c4cf27ffc70d595f4c1121a7786bd18b4233c
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2023-04-28 16:45:57 -0300

    Classe de variáveis globais criada

---
+++ SistemaPagamento/Globals.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalVariables
+{
+    public class Globals
+    {
+        public static List<Cartao> cartoes = new List<Cartao>();
+    }
+}
