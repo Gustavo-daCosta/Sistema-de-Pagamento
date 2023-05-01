eab8f76d3ba1708f6152600e61a6ee1661fda1e4
Guilherme Gozzi Oliveira <olivergozzi@gmail.com>
2023-05-01 19:57:00 -0300

    Criando Classe cartão de crédito

---
+++ SistemaPagamento/CartaoCredito.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace  Cartao
+{
+    public class Credito
+    {
+        public float Limite { get; set; }
+
+        //Parcela com juros Valor não pode ser maior que limite 
+    }
+}
