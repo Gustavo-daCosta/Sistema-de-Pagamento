b206361af106c72e114d4b805fbb18ed8f5a0a17
Richard Felintro <richardfelintro06@gmail.com>
2023-05-01 20:12:53 -0300

    Interação entre a superclasse Cartao e a lista de cartões implementada.

--- SistemaPagamento/Cartao.cs
+++ SistemaPagamento/Cartao.cs
@@ -12,6 +12,17 @@ namespace SistemaPagamento
         public string Titular;
         public string Cvv;
         public abstract void Pagar();
-        public string SalvarCartao();
+        public bool SalvarCartao(string cartaoAnalizado);
+        foreach (Cartao cartaoChecado in cartoes)
+        {
+            if (cartaoChecado == cartaoAnalizado)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
