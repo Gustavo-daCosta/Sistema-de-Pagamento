cfd2fd5d7e1d9bc7286f008378bb8df428cf3e6f
Matheus Lima Cavalcanti <46447568812@info.local>
2023-05-02 15:28:31 -0300

    Novas alteracoes

--- SistemaPagamento/Boleto.cs
+++ SistemaPagamento/Boleto.cs
@@ -1,16 +1,16 @@
-
+using SistemaPagamento;
 
 namespace SistemaPagamento
 {
     public class Boleto : Pagamento
     {
-        private string CodigoDeBarras;
+        private string? CodigoDeBarras;
 
         public void Registrar()
         {
-            Console.WriteLine($"o valor e igual a: {pagamento * (1- 0.12f) }");
+            Console.WriteLine($"o valor e igual a: {Valor * (1- 0.12f) }");
             Random random = new Random();
-            int codigoBarras = random.Next(1, 999999999);
+            this.CodigoDeBarras = random.Next(1, 999999999).ToString();
         }
     }
 }
