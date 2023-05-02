9ad21110e0a31d3c0ea7c34363af37008941a2ae
Mateus Teixeira Paladino <53282675836@info.local>
2023-05-02 16:48:49 -0300

    Classe cartao de debito com alteração no pagar

--- SistemaPagamento/Debito.cs
+++ SistemaPagamento/Debito.cs
@@ -4,18 +4,24 @@ using ClasseCartao;
 
 namespace SistemaPagamento
 {
-    public class Debito : Cartao
+    public class Debito : Cartao 
     {
         private float Saldo;
 
-        public override void Pagar()
-        {
-            throw new NotImplementedException();
-        }
- 
         private float MostrarSaldo()
         {
+            Console.WriteLine($"Informe seu Saldo:");
+            Saldo = float.Parse(Console.ReadLine()!);
+            
+            
             return this.Saldo;
         }
+
+        public override bool Pagar()
+        {
+            float saldo = MostrarSaldo();
+            return saldo >= Valor ? true : false;
+        }
+ 
     }
 }
