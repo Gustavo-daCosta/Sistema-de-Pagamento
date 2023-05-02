13e5f859861f44c8c4fdeb1a0adeeff4580eb07d
Guilherme Gozzi Oliveira <54823324897@info.local>
2023-05-02 15:02:53 -0300

    Terminada a classe Credito

--- SistemaPagamento/CartaoCredito.cs
+++ SistemaPagamento/CartaoCredito.cs
@@ -10,6 +10,34 @@ namespace  CartaoCredito
     {
         public float Limite { get; set; }
 
-        //Parcela com juros Valor não pode ser maior que limite 
+
+
+        public (float, float) PrestacaoJuros(int a , float b, float c){
+            c = 0f;
+            
+
+            if (a <=6)
+            {
+               c = 0.05f; 
+            }
+            else if (c >= 7 && c <=12){
+                c = 0.08f;
+            }
+            
+
+        float prestacao = (b * c) / a;
+        float valortotal = b * c;
+
+        return (prestacao,valortotal);
+        
+
+        }
+
+
+//Parcela com juros Valor não pode ser maior que limite - Crédito :
+// - limite estabelecido no cartão de crédito deve ser pré-definido
+// - máximo de parcelamento 12x
+// - até 6x - acrescentar juros de 5% no valor da compra
+// - entre 7x e 12x acrescentar juros de 8% no valor da compra
     }
 }
