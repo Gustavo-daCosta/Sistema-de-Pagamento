d66a01bea632668850392b970eb51106ffe013e8
Guilherme Gozzi Oliveira <54823324897@info.local>
2023-05-03 15:38:44 -0300

    Alterações mínimas na classe Crédito

--- SistemaPagamento/CartaoCredito.cs
+++ SistemaPagamento/CartaoCredito.cs
@@ -15,27 +15,20 @@ namespace  CartaoCredito
         public override bool Pagar(){
         Console.WriteLine($"Em quantas prestações deseja fazer?");
        
-        int parcela = int.Parse(Console.ReadLine()); 
-        
-        float taxa = 0f;
-            
+        int parcela = int.Parse(Console.ReadLine());           
 
             if (parcela <=6)
             {
                 Console.WriteLine( this.Valor*1.05f);               
                 
             }
-            else if (taxa >= 7 && taxa <=12){
+            else{
                 Console.WriteLine( this.Valor*1.08f);
             }
             
             return parcela <= 12 ? true : false;
 
        
-        
-    
-        
-        
 
         }
         
