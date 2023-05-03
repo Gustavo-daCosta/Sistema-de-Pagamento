c6f96cd0d550db44b0b66ad73397ddca719f7431
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2023-05-03 14:40:17 -0300

    Método Titulo adicionado a classe Globals

--- SistemaPagamento/Globals.cs
+++ SistemaPagamento/Globals.cs
@@ -6,5 +6,12 @@ namespace GlobalVariables
     public class Globals
     {
         public static List<Cartao> cartoes = new List<Cartao>();
+
+        public static void Titulo(string mensagem, ConsoleColor color = ConsoleColor.Blue) {
+            Console.Clear();
+            Console.ForegroundColor = color;
+            Console.WriteLine($" === {mensagem} ===");
+            Console.ResetColor();
+        }
     }
 }
