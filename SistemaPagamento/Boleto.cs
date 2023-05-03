a1b1959f02316b14e8e9c4b7e1c3d90cdca23561
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2023-05-03 16:33:47 -0300

    Classe Boleto modificada

--- SistemaPagamento/Boleto.cs
+++ SistemaPagamento/Boleto.cs
@@ -1,17 +1,51 @@
 using System;
 using SistemaPagamento;
+using GlobalVariables;
+using ClasseFuncionalidades;
+using System.Globalization;
 
-namespace SistemaPagamento
+namespace ClasseBoleto
 {
     public class Boleto : Pagamento
     {
         private string? CodigoDeBarras;
 
-        public void Registrar()
-        {
-            Console.WriteLine($"o valor e igual a: {Valor * (1- 0.12f) }");
+        public void Registrar() {
+            Funcionalidades.Titulo($"Registrar boleto");
+            Boleto boleto = new Boleto();
+
+            Console.Write($"Digite o valor do boleto: R$");
+            boleto.Valor = float.Parse(Console.ReadLine()!);
+
+            boleto.CodigoDeBarras = GerarCodigoDeBarras();
+
+            Console.WriteLine($"\n Dados do boleto:");
+            Console.WriteLine($"Valor: {boleto.Valor.ToString("C2", new CultureInfo("pt-BR"))}");
+            Console.WriteLine($"Data: {boleto.Data}");
+            Console.WriteLine($"Código de barras: {boleto.CodigoDeBarras}");
+
+            Globals.boletos.Add(boleto);
+            Funcionalidades.Mensagem($"Boleto Registrado com sucesso!", ConsoleColor.Green);
+        }
+
+        private string GerarCodigoDeBarras() {
+            string codigoBarras = "";
             Random random = new Random();
-            this.CodigoDeBarras = random.Next(1, 999999999).ToString();
+
+            // 1º Sequência - 10 dígitos
+            codigoBarras += $"{random.NextInt64(1000000000, 9999999999)} ";
+            // 2º Sequência - 10 dígitos
+            codigoBarras += $"{random.NextInt64(1000000000, 9999999999)} ";
+            // 3º Sequência - 10 dígitos
+            codigoBarras += $"{random.NextInt64(1000000000, 9999999999)} ";
+            // 4º Sequência - 1 dígito
+            codigoBarras += $"{random.NextInt64(0, 9)} ";
+            // 5º Sequência - 10 dígitos
+            codigoBarras += $"{random.NextInt64(1000000000, 9999999999)} ";
+            // 6º Sequência - 5 dígitos
+            codigoBarras += random.NextInt64(10000, 99999);
+            
+            return codigoBarras;
         }
     }
 }
